0037dc4e4190a63354c56463d0e661436ea39a5e
RyuukenThoumeaux <133407503+Ryuuken97@users.noreply.github.com>
2024-10-21 03:34:20 +0700

    Update config.js

--- schema/config.js
+++ schema/config.js
@@ -1,15 +1,15 @@
 const options = {
-  name: "HitamAPIs",
-  developer: "@sayuran.vip",
+  name: "Ryuuken APIs",
+  developer: "RyuukenXyz",
   port: 3000,
-  webName: "HitamAPIs Playground",
-  description: "Simple Base Rest API",
+  webName: "Ryuuken APIs Playground",
+  description: "Simple Rest API",
   favicon:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Black.png/220px-Black.png",
 };
 
 const host = {
-  BASE_URL: "https://api-flax-chi-68.vercel.app", // Ganti dengan URL yang sesuai
+  BASE_URL: "https://apis-ryuukenxyz-swagger.vercel.app", // Ganti dengan URL yang sesuai
   // Contoh: https://domain.com
 };
 
